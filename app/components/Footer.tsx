24bff178776cb0be86ed984e57b0d73a3ce65681
Maksym Komarenko <max.komarenko@gmail.com>
2025-11-04 10:08:27 +0200

    Update year in footer's copyright

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -36,7 +36,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 RecipeShare. All rights reserved.</p>
+          <p>&copy; ${new Date().getFullYear()} RecipeShare. All rights reserved.</p>
         </div>
       </div>
     </footer>
